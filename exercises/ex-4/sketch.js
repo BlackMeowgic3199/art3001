3e95c4bd0aa5399abbb29b7fe312d4f589e642f5
Zachary Upperman <upperman.18@osu.edu>
2021-03-21 15:13:37 -0400

    Exercise 4 updates

--- exercises/ex-4/sketch.js
+++ exercises/ex-4/sketch.js
@@ -21,4 +21,54 @@ function _toArray(arr) {
   makeSpans('h1')
   makeSpans('h3')
 
-  
+ // function([string1, string2],target id,[color1,color2])    
+ consoleText(['“It is absolutely imperative that every human being’s freedom and human rights are respected, all over the world.”– Jóhanna Sigurðardóttir', '“Equality means more than passing laws. The struggle is really won in the hearts and minds of the community, where it really counts.” – Barbara Gittings', '“Nature made a mistake, which I have corrected.” – Christine Jorgensen', '“The Lord is my Shepherd and he knows I’m gay.” – Troy Perry', '“The richness, beauty and depths of love can only be fully experienced in a climate of complete openness, honesty and vulnerability.” – Anthony Venn Brown'], 'text',['red','orange','yellow','lightgreen']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
